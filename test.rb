206b35c5c120d652056f0780acd174313b5585a0
TheOtherZach <briggszj@gmail.com>
2013-01-19 15:06:12 -0500

    Redundant test removed

--- test.rb
+++ test.rb
@@ -2,19 +2,6 @@ require 'minitest/autorun'
 require 'turn'
 require_relative 'app.rb'
 
-describe "Sorted List" do
-  before do
-    @list = SortedList.new
-    @list << 3 << 13 << 42 << 4 << 7
-  end
-
-  it "returns each element in order" do
-    results = []
-    @list.each { |e| results << e }
-    results.must_equal [3, 4, 7, 13, 42]
-  end
-end
-
 describe "FakeEnumerable" do
   before do
     @list = SortedList.new
