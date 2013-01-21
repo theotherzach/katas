96e585d4a4b3a73c0d181252be7648dd7f36f612
Bryan Schoeff <schoeff@gmail.com>
2013-01-21 13:20:50 -0500

    skeletoned test suite and fake class for Array

---
+++ lib/fake_array.rb
@@ -0,0 +1,401 @@
+class FakeArray
+  
+  
+  def [] do
+    raise NotImplementedError.new("You need to implement FakeArray.[]")
+  end
+  
+  def try_convert do
+    raise NotImplementedError.new("You need to implement FakeArray.try_convert")
+  end
+  
+  def allocate do
+    raise NotImplementedError.new("You need to implement FakeArray.allocate")
+  end
+  
+  def new do
+    raise NotImplementedError.new("You need to implement FakeArray.new")
+  end
+  
+  def superclass do
+    raise NotImplementedError.new("You need to implement FakeArray.superclass")
+  end
+  
+  def freeze do
+    raise NotImplementedError.new("You need to implement FakeArray.freeze")
+  end
+  
+  def === do
+    raise NotImplementedError.new("You need to implement FakeArray.===")
+  end
+  
+  def == do
+    raise NotImplementedError.new("You need to implement FakeArray.==")
+  end
+  
+  def <=> do
+    raise NotImplementedError.new("You need to implement FakeArray.<=>")
+  end
+  
+  def < do
+    raise NotImplementedError.new("You need to implement FakeArray.<")
+  end
+  
+  def <= do
+    raise NotImplementedError.new("You need to implement FakeArray.<=")
+  end
+  
+  def > do
+    raise NotImplementedError.new("You need to implement FakeArray.>")
+  end
+  
+  def >= do
+    raise NotImplementedError.new("You need to implement FakeArray.>=")
+  end
+  
+  def to_s do
+    raise NotImplementedError.new("You need to implement FakeArray.to_s")
+  end
+  
+  def included_modules do
+    raise NotImplementedError.new("You need to implement FakeArray.included_modules")
+  end
+  
+  def include? do
+    raise NotImplementedError.new("You need to implement FakeArray.include?")
+  end
+  
+  def name do
+    raise NotImplementedError.new("You need to implement FakeArray.name")
+  end
+  
+  def ancestors do
+    raise NotImplementedError.new("You need to implement FakeArray.ancestors")
+  end
+  
+  def instance_methods do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_methods")
+  end
+  
+  def public_instance_methods do
+    raise NotImplementedError.new("You need to implement FakeArray.public_instance_methods")
+  end
+  
+  def protected_instance_methods do
+    raise NotImplementedError.new("You need to implement FakeArray.protected_instance_methods")
+  end
+  
+  def private_instance_methods do
+    raise NotImplementedError.new("You need to implement FakeArray.private_instance_methods")
+  end
+  
+  def constants do
+    raise NotImplementedError.new("You need to implement FakeArray.constants")
+  end
+  
+  def const_get do
+    raise NotImplementedError.new("You need to implement FakeArray.const_get")
+  end
+  
+  def const_set do
+    raise NotImplementedError.new("You need to implement FakeArray.const_set")
+  end
+  
+  def const_defined? do
+    raise NotImplementedError.new("You need to implement FakeArray.const_defined?")
+  end
+  
+  def const_missing do
+    raise NotImplementedError.new("You need to implement FakeArray.const_missing")
+  end
+  
+  def class_variables do
+    raise NotImplementedError.new("You need to implement FakeArray.class_variables")
+  end
+  
+  def remove_class_variable do
+    raise NotImplementedError.new("You need to implement FakeArray.remove_class_variable")
+  end
+  
+  def class_variable_get do
+    raise NotImplementedError.new("You need to implement FakeArray.class_variable_get")
+  end
+  
+  def class_variable_set do
+    raise NotImplementedError.new("You need to implement FakeArray.class_variable_set")
+  end
+  
+  def class_variable_defined? do
+    raise NotImplementedError.new("You need to implement FakeArray.class_variable_defined?")
+  end
+  
+  def public_constant do
+    raise NotImplementedError.new("You need to implement FakeArray.public_constant")
+  end
+  
+  def private_constant do
+    raise NotImplementedError.new("You need to implement FakeArray.private_constant")
+  end
+  
+  def module_exec do
+    raise NotImplementedError.new("You need to implement FakeArray.module_exec")
+  end
+  
+  def class_exec do
+    raise NotImplementedError.new("You need to implement FakeArray.class_exec")
+  end
+  
+  def module_eval do
+    raise NotImplementedError.new("You need to implement FakeArray.module_eval")
+  end
+  
+  def class_eval do
+    raise NotImplementedError.new("You need to implement FakeArray.class_eval")
+  end
+  
+  def method_defined? do
+    raise NotImplementedError.new("You need to implement FakeArray.method_defined?")
+  end
+  
+  def public_method_defined? do
+    raise NotImplementedError.new("You need to implement FakeArray.public_method_defined?")
+  end
+  
+  def private_method_defined? do
+    raise NotImplementedError.new("You need to implement FakeArray.private_method_defined?")
+  end
+  
+  def protected_method_defined? do
+    raise NotImplementedError.new("You need to implement FakeArray.protected_method_defined?")
+  end
+  
+  def public_class_method do
+    raise NotImplementedError.new("You need to implement FakeArray.public_class_method")
+  end
+  
+  def private_class_method do
+    raise NotImplementedError.new("You need to implement FakeArray.private_class_method")
+  end
+  
+  def autoload do
+    raise NotImplementedError.new("You need to implement FakeArray.autoload")
+  end
+  
+  def autoload? do
+    raise NotImplementedError.new("You need to implement FakeArray.autoload?")
+  end
+  
+  def instance_method do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_method")
+  end
+  
+  def public_instance_method do
+    raise NotImplementedError.new("You need to implement FakeArray.public_instance_method")
+  end
+  
+  def nil? do
+    raise NotImplementedError.new("You need to implement FakeArray.nil?")
+  end
+  
+  def =~ do
+    raise NotImplementedError.new("You need to implement FakeArray.=~")
+  end
+  
+  def !~ do
+    raise NotImplementedError.new("You need to implement FakeArray.!~")
+  end
+  
+  def eql? do
+    raise NotImplementedError.new("You need to implement FakeArray.eql?")
+  end
+  
+  def hash do
+    raise NotImplementedError.new("You need to implement FakeArray.hash")
+  end
+  
+  def class do
+    raise NotImplementedError.new("You need to implement FakeArray.class")
+  end
+  
+  def singleton_class do
+    raise NotImplementedError.new("You need to implement FakeArray.singleton_class")
+  end
+  
+  def clone do
+    raise NotImplementedError.new("You need to implement FakeArray.clone")
+  end
+  
+  def dup do
+    raise NotImplementedError.new("You need to implement FakeArray.dup")
+  end
+  
+  def initialize_dup do
+    raise NotImplementedError.new("You need to implement FakeArray.initialize_dup")
+  end
+  
+  def initialize_clone do
+    raise NotImplementedError.new("You need to implement FakeArray.initialize_clone")
+  end
+  
+  def taint do
+    raise NotImplementedError.new("You need to implement FakeArray.taint")
+  end
+  
+  def tainted? do
+    raise NotImplementedError.new("You need to implement FakeArray.tainted?")
+  end
+  
+  def untaint do
+    raise NotImplementedError.new("You need to implement FakeArray.untaint")
+  end
+  
+  def untrust do
+    raise NotImplementedError.new("You need to implement FakeArray.untrust")
+  end
+  
+  def untrusted? do
+    raise NotImplementedError.new("You need to implement FakeArray.untrusted?")
+  end
+  
+  def trust do
+    raise NotImplementedError.new("You need to implement FakeArray.trust")
+  end
+  
+  def frozen? do
+    raise NotImplementedError.new("You need to implement FakeArray.frozen?")
+  end
+  
+  def inspect do
+    raise NotImplementedError.new("You need to implement FakeArray.inspect")
+  end
+  
+  def methods do
+    raise NotImplementedError.new("You need to implement FakeArray.methods")
+  end
+  
+  def singleton_methods do
+    raise NotImplementedError.new("You need to implement FakeArray.singleton_methods")
+  end
+  
+  def protected_methods do
+    raise NotImplementedError.new("You need to implement FakeArray.protected_methods")
+  end
+  
+  def private_methods do
+    raise NotImplementedError.new("You need to implement FakeArray.private_methods")
+  end
+  
+  def public_methods do
+    raise NotImplementedError.new("You need to implement FakeArray.public_methods")
+  end
+  
+  def instance_variables do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_variables")
+  end
+  
+  def instance_variable_get do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_variable_get")
+  end
+  
+  def instance_variable_set do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_variable_set")
+  end
+  
+  def instance_variable_defined? do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_variable_defined?")
+  end
+  
+  def instance_of? do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_of?")
+  end
+  
+  def kind_of? do
+    raise NotImplementedError.new("You need to implement FakeArray.kind_of?")
+  end
+  
+  def is_a? do
+    raise NotImplementedError.new("You need to implement FakeArray.is_a?")
+  end
+  
+  def tap do
+    raise NotImplementedError.new("You need to implement FakeArray.tap")
+  end
+  
+  def send do
+    raise NotImplementedError.new("You need to implement FakeArray.send")
+  end
+  
+  def public_send do
+    raise NotImplementedError.new("You need to implement FakeArray.public_send")
+  end
+  
+  def respond_to? do
+    raise NotImplementedError.new("You need to implement FakeArray.respond_to?")
+  end
+  
+  def respond_to_missing? do
+    raise NotImplementedError.new("You need to implement FakeArray.respond_to_missing?")
+  end
+  
+  def extend do
+    raise NotImplementedError.new("You need to implement FakeArray.extend")
+  end
+  
+  def display do
+    raise NotImplementedError.new("You need to implement FakeArray.display")
+  end
+  
+  def method do
+    raise NotImplementedError.new("You need to implement FakeArray.method")
+  end
+  
+  def public_method do
+    raise NotImplementedError.new("You need to implement FakeArray.public_method")
+  end
+  
+  def define_singleton_method do
+    raise NotImplementedError.new("You need to implement FakeArray.define_singleton_method")
+  end
+  
+  def object_id do
+    raise NotImplementedError.new("You need to implement FakeArray.object_id")
+  end
+  
+  def to_enum do
+    raise NotImplementedError.new("You need to implement FakeArray.to_enum")
+  end
+  
+  def enum_for do
+    raise NotImplementedError.new("You need to implement FakeArray.enum_for")
+  end
+  
+  def equal? do
+    raise NotImplementedError.new("You need to implement FakeArray.equal?")
+  end
+  
+  def ! do
+    raise NotImplementedError.new("You need to implement FakeArray.!")
+  end
+  
+  def != do
+    raise NotImplementedError.new("You need to implement FakeArray.!=")
+  end
+  
+  def instance_eval do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_eval")
+  end
+  
+  def instance_exec do
+    raise NotImplementedError.new("You need to implement FakeArray.instance_exec")
+  end
+  
+  def __send__ do
+    raise NotImplementedError.new("You need to implement FakeArray.__send__")
+  end
+  
+  def __id__ do
+    raise NotImplementedError.new("You need to implement FakeArray.__id__")
+  end
+  
+
+end
---
+++ test.rb
@@ -0,0 +1,405 @@
+require "minitest/autorun"
+
+describe "FakeArray" do
+  before do
+    @list = Array.new
+  end
+
+  
+  it "supports []" do
+    # implement test for []  
+  end
+  
+  it "supports try_convert" do
+    # implement test for try_convert  
+  end
+  
+  it "supports allocate" do
+    # implement test for allocate  
+  end
+  
+  it "supports new" do
+    # implement test for new  
+  end
+  
+  it "supports superclass" do
+    # implement test for superclass  
+  end
+  
+  it "supports freeze" do
+    # implement test for freeze  
+  end
+  
+  it "supports ===" do
+    # implement test for ===  
+  end
+  
+  it "supports ==" do
+    # implement test for ==  
+  end
+  
+  it "supports <=>" do
+    # implement test for <=>  
+  end
+  
+  it "supports <" do
+    # implement test for <  
+  end
+  
+  it "supports <=" do
+    # implement test for <=  
+  end
+  
+  it "supports >" do
+    # implement test for >  
+  end
+  
+  it "supports >=" do
+    # implement test for >=  
+  end
+  
+  it "supports to_s" do
+    # implement test for to_s  
+  end
+  
+  it "supports included_modules" do
+    # implement test for included_modules  
+  end
+  
+  it "supports include?" do
+    # implement test for include?  
+  end
+  
+  it "supports name" do
+    # implement test for name  
+  end
+  
+  it "supports ancestors" do
+    # implement test for ancestors  
+  end
+  
+  it "supports instance_methods" do
+    # implement test for instance_methods  
+  end
+  
+  it "supports public_instance_methods" do
+    # implement test for public_instance_methods  
+  end
+  
+  it "supports protected_instance_methods" do
+    # implement test for protected_instance_methods  
+  end
+  
+  it "supports private_instance_methods" do
+    # implement test for private_instance_methods  
+  end
+  
+  it "supports constants" do
+    # implement test for constants  
+  end
+  
+  it "supports const_get" do
+    # implement test for const_get  
+  end
+  
+  it "supports const_set" do
+    # implement test for const_set  
+  end
+  
+  it "supports const_defined?" do
+    # implement test for const_defined?  
+  end
+  
+  it "supports const_missing" do
+    # implement test for const_missing  
+  end
+  
+  it "supports class_variables" do
+    # implement test for class_variables  
+  end
+  
+  it "supports remove_class_variable" do
+    # implement test for remove_class_variable  
+  end
+  
+  it "supports class_variable_get" do
+    # implement test for class_variable_get  
+  end
+  
+  it "supports class_variable_set" do
+    # implement test for class_variable_set  
+  end
+  
+  it "supports class_variable_defined?" do
+    # implement test for class_variable_defined?  
+  end
+  
+  it "supports public_constant" do
+    # implement test for public_constant  
+  end
+  
+  it "supports private_constant" do
+    # implement test for private_constant  
+  end
+  
+  it "supports module_exec" do
+    # implement test for module_exec  
+  end
+  
+  it "supports class_exec" do
+    # implement test for class_exec  
+  end
+  
+  it "supports module_eval" do
+    # implement test for module_eval  
+  end
+  
+  it "supports class_eval" do
+    # implement test for class_eval  
+  end
+  
+  it "supports method_defined?" do
+    # implement test for method_defined?  
+  end
+  
+  it "supports public_method_defined?" do
+    # implement test for public_method_defined?  
+  end
+  
+  it "supports private_method_defined?" do
+    # implement test for private_method_defined?  
+  end
+  
+  it "supports protected_method_defined?" do
+    # implement test for protected_method_defined?  
+  end
+  
+  it "supports public_class_method" do
+    # implement test for public_class_method  
+  end
+  
+  it "supports private_class_method" do
+    # implement test for private_class_method  
+  end
+  
+  it "supports autoload" do
+    # implement test for autoload  
+  end
+  
+  it "supports autoload?" do
+    # implement test for autoload?  
+  end
+  
+  it "supports instance_method" do
+    # implement test for instance_method  
+  end
+  
+  it "supports public_instance_method" do
+    # implement test for public_instance_method  
+  end
+  
+  it "supports nil?" do
+    # implement test for nil?  
+  end
+  
+  it "supports =~" do
+    # implement test for =~  
+  end
+  
+  it "supports !~" do
+    # implement test for !~  
+  end
+  
+  it "supports eql?" do
+    # implement test for eql?  
+  end
+  
+  it "supports hash" do
+    # implement test for hash  
+  end
+  
+  it "supports class" do
+    # implement test for class  
+  end
+  
+  it "supports singleton_class" do
+    # implement test for singleton_class  
+  end
+  
+  it "supports clone" do
+    # implement test for clone  
+  end
+  
+  it "supports dup" do
+    # implement test for dup  
+  end
+  
+  it "supports initialize_dup" do
+    # implement test for initialize_dup  
+  end
+  
+  it "supports initialize_clone" do
+    # implement test for initialize_clone  
+  end
+  
+  it "supports taint" do
+    # implement test for taint  
+  end
+  
+  it "supports tainted?" do
+    # implement test for tainted?  
+  end
+  
+  it "supports untaint" do
+    # implement test for untaint  
+  end
+  
+  it "supports untrust" do
+    # implement test for untrust  
+  end
+  
+  it "supports untrusted?" do
+    # implement test for untrusted?  
+  end
+  
+  it "supports trust" do
+    # implement test for trust  
+  end
+  
+  it "supports frozen?" do
+    # implement test for frozen?  
+  end
+  
+  it "supports inspect" do
+    # implement test for inspect  
+  end
+  
+  it "supports methods" do
+    # implement test for methods  
+  end
+  
+  it "supports singleton_methods" do
+    # implement test for singleton_methods  
+  end
+  
+  it "supports protected_methods" do
+    # implement test for protected_methods  
+  end
+  
+  it "supports private_methods" do
+    # implement test for private_methods  
+  end
+  
+  it "supports public_methods" do
+    # implement test for public_methods  
+  end
+  
+  it "supports instance_variables" do
+    # implement test for instance_variables  
+  end
+  
+  it "supports instance_variable_get" do
+    # implement test for instance_variable_get  
+  end
+  
+  it "supports instance_variable_set" do
+    # implement test for instance_variable_set  
+  end
+  
+  it "supports instance_variable_defined?" do
+    # implement test for instance_variable_defined?  
+  end
+  
+  it "supports instance_of?" do
+    # implement test for instance_of?  
+  end
+  
+  it "supports kind_of?" do
+    # implement test for kind_of?  
+  end
+  
+  it "supports is_a?" do
+    # implement test for is_a?  
+  end
+  
+  it "supports tap" do
+    # implement test for tap  
+  end
+  
+  it "supports send" do
+    # implement test for send  
+  end
+  
+  it "supports public_send" do
+    # implement test for public_send  
+  end
+  
+  it "supports respond_to?" do
+    # implement test for respond_to?  
+  end
+  
+  it "supports respond_to_missing?" do
+    # implement test for respond_to_missing?  
+  end
+  
+  it "supports extend" do
+    # implement test for extend  
+  end
+  
+  it "supports display" do
+    # implement test for display  
+  end
+  
+  it "supports method" do
+    # implement test for method  
+  end
+  
+  it "supports public_method" do
+    # implement test for public_method  
+  end
+  
+  it "supports define_singleton_method" do
+    # implement test for define_singleton_method  
+  end
+  
+  it "supports object_id" do
+    # implement test for object_id  
+  end
+  
+  it "supports to_enum" do
+    # implement test for to_enum  
+  end
+  
+  it "supports enum_for" do
+    # implement test for enum_for  
+  end
+  
+  it "supports equal?" do
+    # implement test for equal?  
+  end
+  
+  it "supports !" do
+    # implement test for !  
+  end
+  
+  it "supports !=" do
+    # implement test for !=  
+  end
+  
+  it "supports instance_eval" do
+    # implement test for instance_eval  
+  end
+  
+  it "supports instance_exec" do
+    # implement test for instance_exec  
+  end
+  
+  it "supports __send__" do
+    # implement test for __send__  
+  end
+  
+  it "supports __id__" do
+    # implement test for __id__  
+  end
+  
+end
